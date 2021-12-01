d4933968ef414fbfcbbb9b54075fd29592ba977d
Aleksejs Kiselovs <aleksejs.kiselovs@scandiweb.com>
2021-12-01 21:42:07 +0200

    JS start course

---
+++ main.js
@@ -0,0 +1,31 @@
+//Hello
+
+// var car = 10;
+
+let number = 10;
+const apple = 20;
+
+let traktor = "Belarus";
+car = "Volga";
+
+const isGreen = true;
+
+let animal = ["tiger", 12, ["cat"]];
+
+const person = {
+  firstName: "Oleg",
+  lastName: "Dood",
+};
+
+let unknown = null;
+
+console.log(unknown);
+console.log(typeof unknown);
+
+function hello(num1, num2, operator) {
+  let x;
+
+  console.log(x);
+}
+
+hello(animal, number, unknown);
