0405693528af49b628e67fe8a441be56a12ccb4c
Aleksejs Kiselovs <aleksejs.kiselovs@scandiweb.com>
2021-12-09 17:36:03 +0200

    JS - chnages to main.js files

--- main.js
+++ main.js
@@ -2,15 +2,13 @@
 
 // var car = 10;
 
-let number = 10;
+// let number = 10;
 const apple = 20;
 
 let traktor = "Belarus";
 car = "Volga";
 
 const isGreen = true;
-
-let animal = ["tiger", 12, ["cat"]];
 
 const person = {
   firstName: "Oleg",
@@ -19,13 +17,85 @@ const person = {
 
 let unknown = null;
 
-console.log(unknown);
-console.log(typeof unknown);
+function calc(num1, num2, operator) {
+  // if (operator == "/") {
+  //   console.log(num1 / num2);
+  // } else if (operator == "*") {
+  //   console.log(num1 * num2);
+  // } else {
+  //   console.log("Error operator");
+  // }
 
-function hello(num1, num2, operator) {
-  let x;
-
-  console.log(x);
+  switch (operator) {
+    case "+":
+      console.log(num1 + num2);
+      break;
+    case "-":
+      console.log(num1 - num2);
+      break;
+    default:
+      console.log("Error operator");
+  }
 }
 
-hello(animal, number, unknown);
+calc(5, 5, 0);
+
+let animal = ["tiger", "rabit", "cat", "dog", "lion"];
+
+animal.forEach(function (item) {
+  console.log(item + " Nice one");
+});
+
+let counter = animal.length;
+
+console.log(counter);
+
+for (let i = 0; i < counter - 1; i++) {
+  console.log(animal[i]);
+}
+
+let number = 0;
+
+while (number < counter - 1) {
+  console.log(animal[number]);
+  number = number + 1;
+  number++;
+}
+
+let animal = ["tiger", "rabit", "cat", "dog", "lion"];
+animal.forEach(function (item) {
+  console.log(item + " krasava");
+});
+
+let counter = animal.length;
+
+console.log(counter);
+for (let i = 0; i < counter - 1; i++) {
+  console.log(animal[i]);
+}
+
+let number = 0;
+while (number < counter - 1) {
+  console.log(animal[number]);
+  number = number + 1;
+}
+
+let text = "Hello world";
+
+// console.log(text.length);
+
+const cars = "Toyota, Moskvich, Mustang, BMW";
+
+const result = cars.replace("BMW", "MErcedes");
+
+console.log(result);
+
+function replaceText(string) {
+  return string + " Hello";
+}
+
+let results = replaceText("Martinsh");
+
+console.log(results);
+
+alert("HELLO");
